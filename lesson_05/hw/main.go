526eb82bbb75c464699bee4e48afd9cd80b2b57a
d.medvediev <d.medvediev@foodtech.team>
2025-04-27 08:07:55 +0300

    hw lesson 05

--- lesson_05/hw/main.go
+++ lesson_05/hw/main.go
@@ -18,15 +18,33 @@ func main() {
 		return
 	}
 
-	userService := users.Service{usersColl}
+	userService := users.Service{Coll: usersColl}
 
-	userService.CreateUser("1", "Bob")
-	userService.CreateUser("2", "Ned")
+	_, err = userService.CreateUser("1", "Bob")
+	if err != nil {
+		fmt.Printf("cant create new user")
+	}
+
+	_, err = userService.CreateUser("2", "Ned")
+	if err != nil {
+		fmt.Printf("cant create new user")
+	}
 
 	usersList, _ := userService.ListUsers()
-	fmt.Printf("Users collection list: %+v\n", usersList)
+	fmt.Printf("users collection list: %+v\n", usersList)
 
-	userService.DeleteUser("1")
+	err = userService.DeleteUser("1")
+	if err != nil {
+		fmt.Printf("cant delete user")
+	}
+
 	updatedUsersList, _ := userService.ListUsers()
-	fmt.Printf("Users collection list: %+v\n", updatedUsersList)
+	fmt.Printf("users collection list: %+v\n", updatedUsersList)
+
+	user, err := userService.GetUser("1")
+	if err != nil {
+		fmt.Printf("cant get user")
+	} else {
+		fmt.Printf("user: %+v\n", user)
+	}
 }
